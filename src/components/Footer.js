31204f227bbfc21181cf8ff555848258d085a9ed
Clayton Cook <claytonleonardcook@gmail.com>
2021-01-14 21:43:25 -0500

    Edited Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,29 @@ import './Footer.scss';
 function Footer() {
     return (
         <footer>
-            Footer is working...
+            <div>
+                <span>
+                    Related Links:
+            </span>
+                <span>
+                    See DirectKey’s evolution (link to timeline) | Learn more about DirectKey (link to brochure) | Get in touch (contact piece) | LinkedIn Icon (link to linkedin.com)
+            </span>
+            </div>
+            <hr />
+            <div>
+                <img />
+                <div>
+                    <span>
+                        Privacy notice | Terms of use | Vulnerability reporting | A Carrier Company | All Rights Reserved
+                    </span>
+                    <span>
+                        © 2021 Carrier
+                    </span>
+                </div>
+                <img />
+            </div>
         </footer>
-      );
+    );
 }
 
 export default Footer;
