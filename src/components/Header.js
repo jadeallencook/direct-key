3277d0959d47ed1959391a23f61a1be3724303b2
Clayton Cook <claytonleonardcook@gmail.com>
2021-01-14 20:44:22 -0500

    Edited Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,23 @@ function Header() {
             <video>
                 <source src=""></source>
             </video>
+            <h1>
+                100 Million
+            </h1>
+            <span>
+                ...Door Openings and Counting.
+            </span>
+            <span>
+                Direct Key&trade; Mobile Access Solution
+            </span>
+            <p>
+                100 Million. That’s how many times DirectKey has unlocked doors in the past 5 years alone, across 2000 cities globally. Every one of those doors is a cause for confidence for taking your property into the contactless era.
+            </p>
+            <p>
+                DirectKey allows guests to use Bluetooth® technology to unlock the door while approaching it, without touching a phone to the reader - just secure and smooth entry into spaces across the property from the parking garage, to elevators, to guest rooms and beyond.
+            </p>
         </header>
-      );
+    );
 }
 
 export default Header;
