c373f27ba87b9a510cec6a768187e699433ab99c
Clayton Cook <claytonleonardcook@gmail.com>
2021-01-16 00:46:24 -0500

    Added toggle for form

--- src/App.js
+++ src/App.js
@@ -7,26 +7,35 @@ import Stats from './components/Stats';
 import Footer from './components/Footer';
 
 import info from './info';
+import { useState } from 'react';
 
 function App() {
+  const [toggleForm, setToggleForm] = useState(false);
+
   return (
     <div className="App">
-      <Form />
       <Navigation />
-      <Header />
       {
-        info.sections.map(({title, icon, alt, description}, index) => {
-          return <Section
-          title={title}
-          icon={icon}
-          alt={alt}
-          description={description}
-          key={index}
-          />
-        })
+        toggleForm && <Form />
       }
-      <Stats />
-      <Footer />
+      {
+        !toggleForm && <>
+          <Header />
+          {
+            info.sections.map(({ title, icon, alt, description }, index) => {
+              return <Section
+                title={title}
+                icon={icon}
+                alt={alt}
+                description={description}
+                key={index}
+              />
+            })
+          }
+          <Stats />
+          <Footer />
+        </>
+      }
     </div>
   );
 }
