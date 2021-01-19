be4153b2210095b414f53f937776755dd48abd5c
Jade Allen Cook <jadeallencook@gmail.com>
2021-01-19 12:34:03 -0600

    Made final UI adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Footer.scss';
-
+import Carrier from '../assets/carrier-logo.svg';
+import Healthy from '../assets/healthy-buildings.png';
 import LinkedIn from '../assets/linkedin-icon.png';
 
 function Footer() {
@@ -10,44 +11,44 @@ function Footer() {
       <p>
         See DirectKey’s Evolution | Learn more about DirectKey | Get in Touch
       </p>
-      <img src={LinkedIn} alt='linkedin social link' />
+      <a
+        href='https://www.linkedin.com/company/onity'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <img src={LinkedIn} alt='linkedin social link' />
+      </a>
       <hr />
-      <p>
-        <a
-          href='https://www.corporate.carrier.com/legal/privacy-notice'
-          target='_blank'
-          rel='noreferrer'
-        >
-          Privacy Notice
-        </a>
-        {` | `}
-        <a
-          href='https://www.corporate.carrier.com/legal/terms-of-use'
-          target='_blank'
-          rel='noreferrer'
-        >
-          Terms of Use
-        </a>
-        {` | `}
-        <a
-          href='https://www.ccs.utc.com/ccs/en/worldwide/contact-us/vulnerability-reporting/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          Vulnerability Reporting
-        </a>
-        {` | `}
-        <a
-          href='https://www.corporate.carrier.com/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          A Carrier Company
-        </a>
-        {` | `}All Rights Reserved
-        <br />
-        <br />© 2021 Carrier
-      </p>
+      <div>
+        <div>
+          <a
+            href='http://directkey.onity.com/images/logo-carrier.png'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src={Carrier} alt='carrier logo' />
+          </a>
+          <img src={Healthy} alt='healthy buildings logo' />
+        </div>
+        <p>
+          © 2021 Carrier. All Rights Reserved. Bluetooth is a registered
+          trademark of Bluetooth SIG.
+          <a
+            href='https://www.corporate.carrier.com/legal/privacy-notice'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Privacy Notice
+          </a>
+          <a
+            href='https://www.corporate.carrier.com/legal/terms-of-use'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Terms of Use
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
